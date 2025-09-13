92471cb53b2185ea4bc0d4b5e9241cc8484ab58c
Lindsay Webb <lindsayow@gmail.com>
2025-09-13 03:13:45 -0500

    adding ArrayLiterals file with practice solutions

---
+++ ArrayLiterals.js
@@ -0,0 +1,19 @@
+//Task 1: Create Arrays
+//Create two separate arrays using array literals for the following:
+//● An array of 5 grocery store items.
+//● An array of 3 numbers representing prices.
+
+let groceryStoreItems = ["lettuce", "bread", "milk", "eggs", "coffee"];
+let prices = [ 5.00, 3.50, 6.00];
+
+//Task 2: Access and Log the Values
+//Use console.log() to log the arrays.
+//1. Log the entire array of grocery items.
+//2. Log the entire array of prices.
+//3. Log three individual items of your choice.
+//4. Log two prices of your choice.
+
+console.log(groceryStoreItems);
+console.log(prices);
+console.log(groceryStoreItems[1], groceryStoreItems[0], groceryStoreItems[4]);
+console.log(prices[1], prices[0]);
